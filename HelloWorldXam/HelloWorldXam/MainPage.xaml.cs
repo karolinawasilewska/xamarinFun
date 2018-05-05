4fce7ad15ef9ff254b97c32eb9b86fbb298c441a
Karolina <k.wasilewska266@gmail.com>
2018-05-05 15:55:03 +0200

    Add project files.

---
+++ HelloWorldXam/HelloWorldXam/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorldXam
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            slider.Value = 0.5;
+
+
+           Padding= Device.OnPlatform(
+                iOS: new Thickness(0, 20, 0, 0),
+                Android: new Thickness(10, 20, 0, 0),
+                WinPhone: new Thickness(30, 20, 0, 0));
+
+
+            Device.OnPlatform(
+                iOS: () => { Padding = new Thickness(0, 20, 0, 0);
+                });
+		}
+
+       
+    }
+}
---
+++ HelloWorldXam/HelloWorldXam/QuotesPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorldXam
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class QuotesPage : ContentPage
+	{
+        string[] quotes = null;
+		public QuotesPage ()
+		{
+            quotes = new string[] { "quote1", "quote2", "quote3", "quote4", "quote5", "quote6" };
+			InitializeComponent ();
+
+            slider.Maximum = 50.0;
+            slider.Minimum = 16.0;
+
+            quote.Text = quotes[0];
+
+        }
+
+        private void next_Clicked(object sender, EventArgs e)
+        {
+            string current = quote.Text;
+            int index = 0;
+            for (int i = 0; i < quotes.Length; i++)
+            {
+                if (current == quotes[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index>=quotes.Length-1)
+            {
+                index = -1;
+            }
+            quote.Text = quotes[index + 1];
+        }
+    }
+}
